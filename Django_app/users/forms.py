b9fc7a8c1bf0e283dc657fc480ee78bac618d6e4
Felh7 <bakurovnikita7@gmail.com>
2024-07-16 14:19:29 +0300

    Login fix

--- Django_app/users/forms.py
+++ Django_app/users/forms.py
@@ -5,7 +5,7 @@ from django import forms
 class LoginRegisterForm(AuthenticationForm):
     username = forms.CharField(required = True, widget=forms.TextInput(attrs={ 'placeholder': 'Username', 'id': 'usrname', 'minlength': 2, 'required': True}))
     password = forms.CharField(required = True, widget=forms.PasswordInput(attrs={ 'placeholder': 'Password', 'id': 'passwd', 'minlength': 6}))
-    email = forms.CharField(required = True, widget=forms.EmailInput(attrs={ 'placeholder': 'E-mail', 'id': 'e_mail'}))
+   # email = forms.CharField(required = True, widget=forms.EmailInput(attrs={ 'placeholder': 'E-mail', 'id': 'e_mail'}))
     rememberme = forms.BooleanField(label = 'Remeber me', required = False, widget=forms.CheckboxInput())
  
     class Meta:
