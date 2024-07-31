7eca4bfe4dd4ebfaa50b07d769ba994065bfe917
Felh7 <bakurovnikita7@gmail.com>
2024-07-31 22:03:53 +0300

    username authentification check fix

--- Django_app/posts/urls.py
+++ Django_app/posts/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth.views import LogoutView
 app_name = 'posts'
 
 urlpatterns = [
-    path('<str:username>', views.profile_page.as_view(),name='profile'),
     path('addpost', views.post_page.as_view(), name='posts'),
+    path('<str:username>', views.profile_page.as_view(),name='profile'),
 ]
