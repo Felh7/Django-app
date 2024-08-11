30293fd88b3823044c9de22a6afff923a2492d01
Felh7 <bakurovnikita7@gmail.com>
2024-08-11 16:13:23 +0300

    like feature fixed

--- Django_app/static/js/feed_page.js
+++ Django_app/static/js/feed_page.js
@@ -1,5 +1,44 @@
 
-/*$(document).ready(function() {
+$(document).ready(function() {
+    UpdateLikeButtons();
+    $(document).on('htmx:afterSwap', function(event) {
+                UpdateLikeButtons(); 
+    });
+    LikeButtonClickHandle();
+});
+
+function LikeButtonClickHandle(){
+    $(".posts-holder").on("click", ".like-btn, .unlike-btn", function() {
+        var button = $(this);
+            var post_id = button.data('post-id');
+            var url = button.hasClass('like-btn') ? like_ajax_url : unlike_ajax_url;
+            var data = {
+                'csrfmiddlewaretoken': csrftoken,
+                'post_id': post_id,
+            }
+            if (button.hasClass('unlike-btn')) {
+                data.delete = true;
+            }
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: data,
+                success: function(data) {
+                    if (data.liked) {
+                        button.text('Unlike').removeClass('like-btn').addClass('unlike-btn');
+                    }
+                    else {
+                        button.text('Like').removeClass('unlike-btn').addClass('like-btn');
+                    }
+                },
+                error: function(xhr, status, error) {
+                    console.log('error');
+                    console.log(xhr.responseText);
+                }
+            });
+    });
+}
+function UpdateLikeButtons() {
     $(".post-body").each(function() {
         var $this = $(this)
         var $button = $this.find(".like-btn")
@@ -15,43 +54,9 @@
           success: function(response) {
             // Update the like button's state based on the response
             if (response.liked) {
-                $this.find(".like-btn").text('unlike').removeClass('like-btn').addClass("unlike-btn");
+                $this.find(".like-btn").text('Unlike').removeClass('like-btn').addClass("unlike-btn");
             } 
           }
         });
       });
-
-    $('.like-btn, .unlike-btn').click(function() {
-        var button = $(this);
-        var post_id = button.data('post-id');
-        var url = button.hasClass('like-btn') ? like_ajax_url : unlike_ajax_url;
-        var data = {
-            'csrfmiddlewaretoken': csrftoken,
-            'post_id': post_id,
-        }
-        if (button.hasClass('unlike-btn')) {
-            data.delete = true;
-        }
-        $.ajax({
-            type: 'POST',
-            url: url,
-            data: data,
-            success: function(data) {
-                if (data.liked) {
-                    button.text('unlike').removeClass('like-btn').addClass('unlike-btn');
-                }
-                else {
-                    button.text('like').removeClass('unlike-btn').addClass('like-btn');
-                }
-            },
-            error: function(xhr, status, error) {
-                console.log('error');
-                console.log(xhr.responseText);
-            }
-        });
-    });
-});*/
-
-$(".posts-holder").on("click", ".like-btn, .unlike-btn", function() {
-    // Your click event handler code here
-});
+}
